a39973148147d999338792d690d9f1be6d4d31b1
Lerbiq <lerbiq@gmail.com>
2021-12-06 16:08:15 +0100

    More dumb-proofing and remove debug message

--- src/main/java/me/affanhaq/mapcha/handlers/commandHandler.java
+++ src/main/java/me/affanhaq/mapcha/handlers/commandHandler.java
@@ -12,8 +12,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
-
 public class commandHandler implements CommandExecutor {
 
     private Mapcha mapcha;
@@ -32,7 +30,6 @@ public class commandHandler implements CommandExecutor {
 
         if(args.length == 0){
             sender.sendMessage(ChatColor.RED + "You need to fill in the captcha. Look at the map and do the command again like this /captcha <code>");
-            Bukkit.getLogger().info(GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames().toString());
         }else{
             Player player = (Player) sender;
             Bukkit.getLogger().info(player.getDisplayName());
@@ -44,7 +41,7 @@ public class commandHandler implements CommandExecutor {
             }
 
             // captcha success
-            if (args[0].equals(cPlayer.getCaptcha())) {
+            if (args[0].replaceAll("<", "").replaceAll(">", "").equals(cPlayer.getCaptcha())) {
                 Bukkit.getScheduler().runTask(mapcha, () -> Bukkit.getPluginManager().callEvent(new CaptchaSuccessEvent(cPlayer)));
             } else {
                 Bukkit.getScheduler().runTask(mapcha, () -> Bukkit.getPluginManager().callEvent(new CaptchaFailedEvent(cPlayer)));
